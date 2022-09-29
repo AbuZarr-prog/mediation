7d477d10343299d2ec09f8f17008062fe4f465b7
AbuZarr-prog <tim.abu.zarr.9@gmail.com>
2022-09-29 09:13:37 +0600

    Redesign

--- js/app.js
+++ js/app.js
@@ -1,54 +1,5 @@
-$(function(){
-	   /*	Sticky Navigation
-	------------------------------------------------------*/
-   window.addEventListener("scroll", function(){
-   		var header = document.querySelector("header");
-   		 header.classList.toggle("sticky", window.scrollY > 0);
-   });
+window.addEventListener("scroll", function(){
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
 
-
-	/*----------------------------------------------------*/
-  	/* Smooth Scrolling
-  	------------------------------------------------------*/
-  	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
-
-   	var target = this.hash,
-    	$target = $(target);
-
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      	}, 800, 'swing', function () {
-      	window.location.hash = target;
-      	});
-
-  	});
-
-    /* Highlight the current section in the navigation bar
-  	------------------------------------------------------*/
-	var sections = $("section"),
-	navigation_links = $("#nav .button a");	
-
-	sections.waypoint( {
-
-      handler: function(direction) {
-
-		   	var active_section;
-
-				active_section = $('.section' + this.element.id);
-
-				if (direction === "up") active_section = active_section.prev();
-
-				var active_link = $('#nav a[href="#' + active_section.attr("id") + '"]');			
-
-				navigation_links.parent().removeClass("current");
-				active_link.parent().addClass("current");
-
-			}, 
-
-		offset: '25%'
-
-	});
-
-});
